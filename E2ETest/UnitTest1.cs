488ae6a01d90e703dbeebf42ebb2c2d1c92ef856
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-01-28 10:40:08 +0900

    E2Etest addDialog

--- E2ETest/UnitTest1.cs
+++ E2ETest/UnitTest1.cs
@@ -30,4 +30,32 @@ public class Tests : PageTest
 
 
     }
+
+    [Test]
+    public async Task AddDialog()
+    {
+        await Page.GotoAsync("https://localhost:7283/");
+
+        await Expect(Page).ToHaveTitleAsync(new Regex("Index"));
+
+        await Page.GetByTestId("pagetitle").IsVisibleAsync();
+
+        await Page.GetByTestId("addbutton").IsVisibleAsync();
+
+        await Page.GetByTestId("addbutton").ClickAsync();
+
+        await Page.GetByTestId("adddialogpanetitle").IsVisibleAsync();
+        await Page.GetByTestId("adddialogtitlelabel").IsVisibleAsync();
+        await Page.GetByTestId("adddialogtitletextfield").IsVisibleAsync();
+        await Page.GetByTestId("adddialogcontentlabel").IsVisibleAsync();
+        await Page.GetByTestId("adddialogcontenttextfield").IsVisibleAsync();
+        await Page.GetByTestId("adddialogauthorlabel").IsVisibleAsync();
+        await Page.GetByTestId("adddialogauthortextfield").IsVisibleAsync();
+        await Page.GetByTestId("adddialogcreatedlabel").IsVisibleAsync();
+        await Page.GetByTestId("adddialogcreatedtextfield").IsVisibleAsync();
+        await Page.GetByTestId("adddialogupdatedlabel").IsVisibleAsync();
+        await Page.GetByTestId("adddialogupdatedtextfield").IsVisibleAsync();
+        await Page.GetByTestId("adddialogsubmit").IsVisibleAsync();
+        await Page.GetByTestId("adddialogclose").IsVisibleAsync();
+    }
 }
